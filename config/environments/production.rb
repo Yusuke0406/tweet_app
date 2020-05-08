f998ff5904756e690e5f812e1199a5a56e2428a1
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-05-08 16:01:23 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.action_mailer.default_url_options = { host: 'example.com'}
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
   Rails.application.routes.default_url_options[:host] = 'domain.com'
   # Do not fallback to assets pipeline if a precompiled asset is missed.
