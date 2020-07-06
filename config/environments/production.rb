0ca8f46ab242696636971022a3b7e40bea7a04ad
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-07-06 22:24:56 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
 
   Rails.application.configure do
-    config.action_mailer.default_url_options = { :host => 'EC2で発行してもらったIPアドレス' }
+    config.action_mailer.default_url_options = { :host => '54.150.205.107' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
