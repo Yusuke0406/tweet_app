be4e3ae18fb87dfa8ed2aca69fa7225c17262080
Yusuke Harada <lefty0406_0513@yahoo.co.jp>
2020-07-07 16:12:36 +0900

    モーダル機能の追加

--- app/assets/javascripts/twitter.js
+++ app/assets/javascripts/twitter.js
@@ -1,17 +1,8 @@
-// $(function() {
-//   let tabs = $(".show__tab");
-
-//   function tabSwitch(){
-    
-//     $(".active").removeClass("active");
-
-//     $(this).addClass("active");
-
-//     const index = tabs.index(this);
-
-//     $(".show__inside").removeClass("show").eq(index).addClass("show");
-
-//     return false;
-//   }
-//   tabs.click(tabSwitch);
-// });
+$(function() {
+  $('#user_icon').click(function(){
+    $('.sign_modal_wrapper').fadeIn();
+  })
+  $('.close-modal').click(function(){
+    $('.sign_modal_wrapper').fadeOut();
+  });
+});
